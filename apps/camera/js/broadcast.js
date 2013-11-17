dc6f64a72a9d0250a67c7ed12e834515b3dd8a6d
Wilson Page <wilsonpage@me.com>
2013-11-17 10:12:31 -0800

    Broadcast no longer needs to be a global

--- apps/camera/js/broadcast.js
+++ apps/camera/js/broadcast.js
@@ -1,12 +1,5 @@
 
 define(function(require) {
   'use strict';
-  var exports = require('libs/evt').mix({});
-
-  // We have to store on window
-  // temporarily to allow camera.js
-  // to listen to and boradcast events.
-  window.broadcast = exports;
-
-  return exports;
+  return require('libs/evt').mix({});
 });
