bdc72bef2453253767b2e848a32aac90b7833332
Wilson Page <wilsonpage@me.com>
2013-11-18 19:29:04 -0800

    Rework how button state is managed

--- apps/camera/js/controllers/controls.js
+++ apps/camera/js/controllers/controls.js
@@ -13,10 +13,27 @@ define(function(require) {
     // Bind events
     camera.on('captureModeChange', onCameraModeChange);
     camera.on('videoTimeUpdate', onVideoTimeUpdate);
+    camera.on('preparingToTakePicture', controls.disableButtons);
+    camera.on('previewResumed', controls.enableButtons);
+    camera.on('focusFailed', controls.enableButtons);
     controls.on('modeButtonToggle', onModeButtonToggle);
 
+    // Set initial state
+    var mode = camera.getMode();
+    var isCancellable = activity.name === 'pick';
+    var showCamera = !activity.active || activity.allowedTypes.image;
+    var showVideo = !activity.active || activity.allowedTypes.video;
+    var isSwitchable = showVideo && showCamera;
+    var showGallery = !activity.active;
+
+    controls.set('mode', mode);
+    controls.set('gallery', showGallery);
+    controls.set('cancel', isCancellable);
+    controls.set('camera', showCamera);
+    controls.set('switchable', isSwitchable);
+
     function onCameraModeChange(mode) {
-      controls.setCaptureMode(mode);
+      controls.set('mode', mode);
     }
 
     function onVideoTimeUpdate(value) {
