344804f6a9fe3db81fef7827a7186415d1d9336c
Wilson Page <wilsonpage@me.com>
2013-11-18 19:40:23 -0800

    Remvoe dead code

--- apps/camera/js/views/controls.js
+++ apps/camera/js/views/controls.js
@@ -8,26 +8,6 @@ define(function(require) {
   var find = require('utils/find');
   var camera = require('camera');
 
-  var setBooleanAttribute = function(el, attribute, value) {
-    if (value) {
-      el.setAttribute(attribute, attribute);
-    }
-
-    else {
-      el.removeAttribute(attribute);
-    }
-  };
-
-  var setBooleanClass = function(el, className, value) {
-    if (value) {
-      el.classList.add(className);
-    }
-
-    else {
-      el.classList.remove(className);
-    }
-  };
-
   return View.extend({
     buttonsDisabledClass: 'buttons-disabled',
     initialize: function() {
