078ee6f87027e9fee7d868d2a8e6620ad2caa88b
Justin D'Arcangelo <justindarc@gmail.com>
2013-12-09 16:16:43 -0500

    Updated basic functional Python tests to work with Camera refactor.

--- tests/python/gaia-ui-tests/gaiatest/apps/camera/app.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/camera/app.py
@@ -13,17 +13,29 @@ class Camera(Base):
 
     name = 'Camera'
 
-    _capture_button_locator = (By.CSS_SELECTOR, '#capture-button:not([disabled])')
-    _filmstrip_thumbnail_locator = (By.CSS_SELECTOR, '#filmstrip > img.thumbnail')
-    _switch_source_button_locator = (By.ID, 'switch-button')
-    _video_timer_locator = (By.ID, 'video-timer')
-    _filmstrip_locator = (By.ID, 'filmstrip')
-    _body_locator = (By.TAG_NAME, 'body')
-    _gallery_button_locator = (By.ID, 'gallery-button')
-
-    # locators for the case when other apps open camera app
-    _select_button_locator = (By.ID, 'select-button')
     _camera_frame_locator = (By.CSS_SELECTOR, 'iframe[src*="camera"][src*="/index.html"]')
+    _body_locator = (By.TAG_NAME, 'body')
+
+    # Controls View
+    _controls_locator = (By.CSS_SELECTOR, '.js-controls')
+    _controls_enabled_locator = (By.CSS_SELECTOR, '.js-controls:not(.buttons-disabled)')
+    _switch_button_locator = (By.CSS_SELECTOR, '.js-switch')
+    _capture_button_locator = (By.CSS_SELECTOR, '.js-capture')
+    _gallery_button_locator = (By.CSS_SELECTOR, '.js-gallery')
+    _cancel_pick_button_locator = (By.CSS_SELECTOR, '.js-cancel-pick')
+    _video_timer_locator = (By.CSS_SELECTOR, '.js-video-timer')
+
+    # FocusRing View
+    _focus_ring_locator = (By.CSS_SELECTOR, '.focus-ring')
+
+    # Filmstrip View
+    _filmstrip_locator = (By.ID, 'filmstrip')
+    _filmstrip_image_locator = (By.CSS_SELECTOR, '#filmstrip > img.thumbnail')
+    _filmstrip_visible_locator = (By.CSS_SELECTOR, 'body:not(.filmstriphidden)')
+    _filmstrip_hidden_locator = (By.CSS_SELECTOR, 'body.filmstriphidden')
+
+    # ConfirmDialog
+    _select_button_locator = (By.ID, 'select-button')
 
     def launch(self):
         Base.launch(self)
@@ -35,20 +47,23 @@ class Camera(Base):
         # Wait for filmstrip to appear
         self.wait_for_filmstrip_visible()
 
-        # Wait for filmstrip to clear
-        self.wait_for_filmstrip_not_visible()
-
     def record_video(self, duration):
         # Start recording
         self.tap_capture()
+
+        # Wait for Controls View state to indicate that recording is in-progress
         self.wait_for_video_capturing()
+
         # Wait for duration
         timer_text = "00:%02d" % duration
         self.wait_for_condition(lambda m: m.find_element(
-            *self._video_timer_locator).text == timer_text, timeout=duration + 30)
+            *self._video_timer_locator).text >= timer_text, timeout=duration + 10)
+
         # Stop recording
         self.tap_capture()
-        self.wait_for_element_not_displayed(*self._video_timer_locator)
+
+        # Wait for filmstrip to appear
+        self.wait_for_filmstrip_visible()
 
     def tap_capture(self):
         self.wait_for_camera_ready()
@@ -58,7 +73,8 @@ class Camera(Base):
         self.marionette.find_element(*self._select_button_locator).tap()
 
     def tap_switch_source(self):
-        self.marionette.find_element(*self._switch_source_button_locator).tap()
+        self.marionette.find_element(*self._switch_button_locator).tap()
+        self.wait_for_capture_ready()
 
     def tap_to_display_filmstrip(self):
         self.marionette.find_element(*self._body_locator).tap()
@@ -68,18 +84,19 @@ class Camera(Base):
         self.wait_for_element_displayed(*self._select_button_locator)
 
     def wait_for_camera_ready(self):
-        self.wait_for_element_displayed(*self._capture_button_locator)
+        self.wait_for_element_present(*self._controls_enabled_locator)
 
     def wait_for_filmstrip_visible(self):
         self.wait_for_condition(lambda m: self.is_filmstrip_visible)
 
     def wait_for_filmstrip_not_visible(self):
-        filmstrip = self.marionette.find_element(*self._filmstrip_locator)
-        self.wait_for_condition(lambda m: filmstrip.location['y'] == (0 - filmstrip.size['height']), timeout=10)
+        self.wait_for_condition(lambda m: self.is_filmstrip_hidden)
+
+    def wait_for_capture_ready(self):
+        self.wait_for_condition(lambda m: self.marionette.find_element(*self._focus_ring_locator).get_attribute('data-state') is None)
 
     def wait_for_video_capturing(self):
-        body = self.marionette.find_element(*self._body_locator)
-        self.wait_for_condition(lambda m: 'capturing' in body.get_attribute('class'))
+        self.wait_for_condition(lambda m: self.marionette.find_element(*self._controls_locator).get_attribute('data-recording') == 'true')
 
     def wait_for_video_timer_not_visible(self):
         self.wait_for_element_not_displayed(*self._video_timer_locator)
@@ -101,7 +118,11 @@ class Camera(Base):
 
     @property
     def is_filmstrip_visible(self):
-        return self.marionette.find_element(*self._filmstrip_locator).location['y'] == 0
+        return self.is_element_present(*self._filmstrip_visible_locator)
+
+    @property
+    def is_filmstrip_hidden(self):
+        return self.is_element_present(*self._filmstrip_hidden_locator)
 
     @property
     def video_timer(self):
