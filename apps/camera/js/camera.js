wilsonpage/gaia

commit 4dd14aecad82029b9d7bbac9a8a0e198534b5639

Docs and tidy

apps/camera/js/camera.js
@@ -1,4 +1,5 @@
 define(function(require, exports, module){
+/*global CONFIG_MAX_IMAGE_PIXEL_SIZE*/
 /*jshint laxbreak:true*/
 
 'use strict';
@@ -136,18 +137,42 @@ proto.configureStorage = function() {
   this._pictureStorage.addEventListener('change', this.onStorageChange);
 };
 
+/**
+ * Returns the current
+ * capture mode.
+ *
+ * @return {String} 'camera'|'video'
+ */
 proto.getMode = function() {
   return this.state.get('mode');
-},
+};
 
+/**
+ * States if the camera is
+ * in 'camera' capture mode.
+ *
+ * @return {Boolean}
+ */
 proto.isCameraMode = function() {
   return this.getMode() === CAMERA;
 };
 
+/**
+ * States if the camera is
+ * in 'video' capture mode.
+ *
+ * @return {Boolean}
+ */
 proto.isVideoMode = function() {
   return this.getMode() === VIDEO;
 };
 
+/**
+ * Toggles between 'camera'
+ * and 'video' capture modes.
+ *
+ * @return {String}
+ */
 proto.toggleMode = function() {
   var newMode = this.isCameraMode()
     ? VIDEO
@@ -155,19 +180,42 @@ proto.toggleMode = function() {
 
   this.setCaptureMode(newMode);
   this.configureFlashModes(this.flash.all);
+  return newMode;
 },
 
+/**
+ * Sets the capture mode.
+ *
+ * @param {String} mode
+ * @returns {String}
+ */
 proto.setCaptureMode = function(mode) {
   this.state.set('mode', mode);
   this.emit('captureModeChange', mode);
   return mode;
 };
 
+/**
+ * Toggles the camera number
+ * between back (0) and front(1).
+ *
+ * @return {Number}
+ */
 proto.toggleCamera = function() {
   var cameraNumber = 1 - this.state.get('cameraNumber');
   this.state.set('cameraNumber', cameraNumber);
-},
+  return cameraNumber;
+};
 
+/**
+ * Cycles through flash
+ * modes available for the
+ * current camera (0/1) and
+ * capture mode ('camera'/'video')
+ * combination.
+ *
+ * @return {String}
+ */
 proto.toggleFlash = function() {
   var available = this.flash.available;
   var current = this.flash.current;
@@ -177,18 +225,41 @@ proto.toggleFlash = function() {
 
   this.setFlashMode(next);
   return name;
-},
+};
 
+/**
+ * Gets the name of the
+ * current flash mode.
+ *
+ * @return {String}
+ */
 proto.getFlashMode = function() {
   var index = this.flash.current;
   return this.flash.available[index];
-},
+};
 
+/**
+ * Sets the current flash mode,
+ * both on the Camera instance
+ * and on the cameraObj hardware.
+ *
+ * @param {Number} index
+ */
 proto.setFlashMode = function(index) {
   var name = this.flash.available[index];
   this._cameraObj.flashMode = name;
   this.flash.current = index;
-},
+};
+
+/**
+ * States if the current
+ * device has a front camera.
+ *
+ * @return {Boolean}
+ */
+proto.hasFrontCamera = function() {
+  return this.numCameras > 1;
+};
 
 proto.setFocusMode = function() {
   this._callAutoFocus = false;
@@ -214,18 +285,34 @@ proto.setFocusMode = function() {
   }
 },
 
-proto.capture = function() {
+/**
+ * Takes a photo, or begins/ends
+ * a video capture session.
+ *
+ * Options:
+ *
+ *   - `position` {Object} - geolocation to store in EXIF
+ *
+ * @param  {Object} options
+ * @api public
+ */
+proto.capture = function(options) {
+  var self = this;
 
   // Camera
   if (this.isCameraMode()) {
-    this.prepareTakePicture();
-    return;
+    this.prepareTakePicture(function() {
+      self.takePicture(options);
+    });
   }
 
-  // Video
-  if (this.state.get('recording')) {
+  // Video (stop)
+  else if (this.state.get('recording')) {
     this.stopRecording();
-  } else {
+  }
+
+  // Video (start)
+  else {
     this.startRecording();
   }
 },
@@ -357,6 +444,12 @@ proto.startRecordingTimer = function() {
   updateVideoTimer();
 },
 
+/**
+ * TODO: Use the camera.state object
+ * to store elapsed video time. String
+ * formatting should be happening
+ * in the controls view.
+ */
 proto.updateVideoTimer = function() {
   var timestamp = new Date().getTime();
   var ms = timestamp - this._videoStart;
@@ -525,6 +618,7 @@ proto.saveVideoPosterImage = function(filename, callback) {
   }
 };
 
+// TODO: Move this out into a util
 proto.formatTimer = function(time) {
   var minutes = Math.floor(time / 60);
   var seconds = Math.round(time % 60);
@@ -663,10 +757,6 @@ proto.recordingStateChanged = function(msg) {
     alert(navigator.mozL10n.get(alertText));
     this.sizeLimitAlertActive = false;
   }
-};
-
-proto.hasFrontCamera = function() {
-  return this.numCameras > 1;
 };
 
 proto.configureFlashModes = function(allModes) {
@@ -907,15 +997,21 @@ proto.setStorageState = function(value) {
   this.state.set('storage', value);
 };
 
-proto.prepareTakePicture = function() {
+proto.prepareTakePicture = function(done) {
+  var self = this;
+
   this.emit('preparingToTakePicture');
 
-  if (this._callAutoFocus) {
-    this.state.set('focusState', 'focusing');
-    this._cameraObj.autoFocus(this.autoFocusDone.bind(this));
-  } else {
-    this.takePicture();
+  if (!this._callAutoFocus) {
+    done();
+    return;
   }
+
+  this.state.set('focusState', 'focusing');
+  this._cameraObj.autoFocus(function() {
+    self.autoFocusDone();
+    done();
+  });
 };
 
 proto.autoFocusDone = function(success) {
@@ -926,27 +1022,24 @@ proto.autoFocusDone = function(success) {
   }
 
   this.state.set('focusState', 'focused');
-  this.takePicture();
 };
 
-proto.takePicture = function() {
+proto.takePicture = function(options) {
+  var position = options && options.position;
   var config = {
     rotation: window.orientation.get(),
     dateTime: Date.now() / 1000,
     fileFormat: this.fileFormat
   };
 
-  this._cameraObj.pictureSize = this._pictureSize;
-
-  // We do not attach our current
-  // position to the exif of photos
-  // that are taken via an activity.
-  //
-  // As it leaks position information
-  // to other apps without permission
-  if (this._position && !this._pendingPick) {
-    config.position = this._position;
+  // If position has been
+  // passed in, add it to
+  // the config object.
+  if (position) {
+    config.position = position;
   }
+
+  this._cameraObj.pictureSize = this._pictureSize;
 
   this._cameraObj.takePicture(
     config,
@@ -1122,29 +1215,15 @@ proto.pickVideoProfile = function(profiles) {
   };
 };
 
-proto.initPositionUpdate = function() {
-  if (this._watchId || document.hidden) {
-    return;
-  }
-  this._watchId = navigator.geolocation
-    .watchPosition(this.updatePosition.bind(this));
-};
+/**
+ * Releases the camera hardware.
+ *
+ * @param  {Function} done
+ * @api public
+ */
+proto.release = function(done) {
+  done = done || function(){};
 
-proto.updatePosition = function(position) {
-  this._position = {
-    timestamp: position.timestamp,
-    altitude: position.coords.altitude,
-    latitude: position.coords.latitude,
-    longitude: position.coords.longitude
-  };
-};
-
-proto.cancelPositionUpdate = function() {
-  navigator.geolocation.clearWatch(this._watchId);
-  this._watchId = null;
-};
-
-proto.release = function(callback) {
   if (!this._cameraObj) {
     return;
   }
@@ -1154,34 +1233,30 @@ proto.release = function(callback) {
 
   function onSuccess() {
     self._cameraObj = null;
-    if (callback) {
-      callback();
-    }
+    done();
   }
 
   function onError() {
     console.warn('Camera: failed to release hardware?');
-    if (callback) {
-      callback();
-    }
+    done();
   }
 };
 
-proto.getPreferredSizes = function(callback) {
+proto.getPreferredSizes = function(done) {
+  done = done || function(){};
+
   var key = 'camera.recording.preferredSizes';
   var self = this;
 
-  if (this.preferredRecordingSizes && callback) {
-    callback();
+  if (this.preferredRecordingSizes) {
+    done(this.preferredRecordingSizes);
     return;
   }
 
   var req = navigator.mozSettings.createLock().get(key);
   req.onsuccess = function() {
     self.preferredRecordingSizes = req.result[key] || [];
-    if (callback) {
-      callback();
-    }
+    done(self.preferredRecordingSizes);
   };
 };
 

commit 67437ea500ddc23d98ed08ac3eb956ae49fc09ea

Fixed autofocus issue introduced from commit 4dd14ae.

apps/camera/js/camera.js
@@ -65,7 +65,6 @@ function Camera() {
   // video root directory string
   this._videoRootDir = null;
   this._autoFocusSupport = {};
-  this._callAutoFocus = false;
   this._cameraObj = null;
   this._pictureSize = null;
   this._previewConfig = null;
@@ -961,36 +960,27 @@ proto.prepareTakePicture = function(done) {
 
   this.emit('preparingToTakePicture');
 
-  if (this._autoFocusSupport[FOCUS_MODE_TYPE.MANUALLY_TRIGGERED]) {
-    this.state.set('focusState', 'focusing');
-    this._cameraObj.autoFocus(this.autoFocusDone.bind(this));
-  } else {
-    this.takePicture();
-  }
-
-  this.state.set('focusState', 'focusing');
-  this._cameraObj.autoFocus(function() {
-    self.autoFocusDone();
+  if (!this._autoFocusSupport[FOCUS_MODE_TYPE.MANUALLY_TRIGGERED]) {
     done();
-  });
-};
-
-proto.autoFocusDone = function(success) {
-  var state = this.state;
-
-  if (!success) {
-    state.set('focusState', 'fail');
-    this.emit('focusFailed');
-
-    window.setTimeout(function() {
-      state.set('focusState', 'none');
-    }, 1000);
-
     return;
   }
 
-  state.set('focusState', 'focused');
-  this.takePicture();
+  this.state.set('focusState', 'focusing');
+  this._cameraObj.autoFocus(function(success) {
+    if (!success) {
+      self.state.set('focusState', 'fail');
+      self.emit('focusFailed');
+
+      window.setTimeout(function() {
+        self.state.set('focusState', 'none');
+      }, 1000);
+
+      return;
+    }
+
+    self.state.set('focusState', 'focused');
+    done();
+  });
 };
 
 proto.takePicture = function(options) {

apps/camera/js/views/focusring.js
@@ -7,7 +7,7 @@ define(function(require) {
   return View.extend({
     className: 'focus-ring',
     setState: function(state) {
-      this.el.setAttribute('data-state', state.value);
+      this.el.setAttribute('data-state', state);
     }
   });
 });