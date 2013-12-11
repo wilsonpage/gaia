1b3a2ea7a6dd90d81d63e355aaf6596008c30a0d
Wilson Page <wilsonpage@me.com>
2013-12-11 15:56:48 +0000

    Fully tested HudController

--- apps/camera/js/controllers/hud.js
+++ apps/camera/js/controllers/hud.js
@@ -11,13 +11,30 @@ var proto = HudController.prototype;
  * Exports
  */
 
-module.exports = HudController;
+exports = module.exports = create;
+exports.HudController = HudController;
 
+/**
+ * Create new `HudController`
+ * and bind events.
+ *
+ * @param  {AppController} app
+ * @return {HudController}
+ * @api public
+ */
+function create(app) {
+  return new HudController(app)
+    .bindEvents();
+}
+
+/**
+ * Initialize a new `HudController`
+ *
+ * @param {AppController} app
+ * @constructor
+ * @api private
+ */
 function HudController(app) {
-  if (!(this instanceof HudController)) {
-    return new HudController(app);
-  }
-
   this.viewfinder = app.views.viewfinder;
   this.controls = app.views.controls;
   this.hud = app.views.hud;
@@ -28,10 +45,13 @@ function HudController(app) {
   this.onCameraToggle = this.onCameraToggle.bind(this);
   this.onCameraConfigured = this.onCameraConfigured.bind(this);
   this.onRecordingChange = this.onRecordingChange.bind(this);
-
-  this.bindEvents();
 }
 
+/**
+ * Bind callbacks to events.
+ *
+ * @api private
+ */
 proto.bindEvents = function() {
   this.hud.on('flashToggle', this.onFlashToggle);
   this.hud.on('cameraToggle', this.onCameraToggle);
@@ -41,6 +61,12 @@ proto.bindEvents = function() {
   this.camera.state.on('change:recording', this.onRecordingChange);
 };
 
+/**
+ * Update UI when a new
+ * camera is configured.
+ *
+ * @api private
+ */
 proto.onCameraConfigured = function() {
   var hasFrontCamera = this.camera.hasFrontCamera();
   var flashMode = this.camera.getFlashMode();
@@ -48,11 +74,24 @@ proto.onCameraConfigured = function() {
   this.hud.setFlashMode(flashMode);
 };
 
+/**
+ * Toggles the flash on
+ * the camera and UI when
+ * the flash button is pressed.
+ *
+ * @api private
+ */
 proto.onFlashToggle = function() {
   var mode = this.camera.toggleFlash();
   this.hud.setFlashMode(mode);
 };
 
+/**
+ * Toggle the camera (front/back),
+ * fading the viewfinder in between.
+ *
+ * @api private
+ */
 proto.onCameraToggle = function() {
   var controls = this.controls;
   var viewfinder = this.viewfinder;
@@ -77,6 +116,13 @@ proto.onCameraToggle = function() {
   }
 };
 
+/**
+ * Disable the buttons
+ * when recording
+ *
+ * @param  {Boolean} value
+ * @api private
+ */
 proto.onRecordingChange = function(value) {
   this.hud.toggleDisableButtons(value);
 };
