21c920f0287e36b3b899f18178702ec9a4677af0
Wilson Page <wilsonpage@me.com>
2013-12-12 16:24:26 +0000

    Consistent module indentation, update require paths

--- apps/camera/js/views/overlay.js
+++ apps/camera/js/views/overlay.js
@@ -1,63 +1,72 @@
 define(function(require, exports, module) {
-  'use strict';
+'use strict';
 
-  var View = require('view');
-  var find = require('utils/find');
-  var bind = require('utils/bind');
+/**
+ * Dependencies
+ */
 
-  module.exports = View.extend({
-    className: 'overlay',
+var View = require('libs/view');
+var find = require('utils/find');
+var bind = require('utils/bind');
 
-    initialize: function(options) {
-      this.model = options.data;
-      this.data('type', options.type);
-      this.data('closable', options.closable);
-      this.render();
-    },
+/**
+ * Exports
+ */
 
-    render: function() {
+module.exports = View.extend({
+  className: 'overlay',
 
-      // Inject HTML
-      this.el.innerHTML = this.template(this.model);
+  initialize: function(options) {
+    this.model = options.data;
+    this.data('type', options.type);
+    this.data('closable', options.closable);
+    this.render();
+  },
 
-      // Pick out elements
-      this.els.buttons = {
-        storage: find('.js-storage-btn', this.el),
-        close: find('.js-close-btn', this.el)
-      };
+  render: function() {
 
-      // Attach event listeners
-      bind(this.els.buttons.storage, 'click', this.onButtonClick, this);
-      bind(this.els.buttons.close, 'click', this.onButtonClick, this);
-    },
+    // Inject HTML
+    this.el.innerHTML = this.template(this.model);
 
-    template: function(data) {
-      /*jshint maxlen:false*/
-      return '<form role="dialog" data-type="confirm">' +
-        '<section>' +
-          '<h1 class="overlay-title">' + data.title + '</h1>' +
-          '<p id="overlay-text">' + data.body + '<p>' +
-        '</section>' +
-        '<menu class="overlay-menu-close">' +
-          '<button class="full js-close-btn" type="button" name="close-btn">' + data.closeButtonText + '</button>' +
-        '</menu>' +
-        '<menu class="overlay-menu-storage">' +
-          '<button class="full js-storage-btn" type="button" name="storage-settings-btn">' + data.storageButtonText + '</button>' +
-        '</menu>' +
-      '</form>';
-    },
+    // Pick out elements
+    this.els.buttons = {
+      storage: find('.js-storage-btn', this.el),
+      close: find('.js-close-btn', this.el)
+    };
 
-    data: function(key, value) {
-      switch (arguments.length) {
-        case 1: return this.el.getAttribute('data-' + key);
-        case 2: this.el.setAttribute('data-' + key, value);
-      }
-    },
+    // Attach event listeners
+    bind(this.els.buttons.storage, 'click', this.onButtonClick, this);
+    bind(this.els.buttons.close, 'click', this.onButtonClick, this);
+  },
 
-    onButtonClick: function(event) {
-      var el  = event.currentTarget;
-      var name = el.getAttribute('name');
-      this.emit('click:' + name);
+  template: function(data) {
+    /*jshint maxlen:false*/
+    return '<form role="dialog" data-type="confirm">' +
+      '<section>' +
+        '<h1 class="overlay-title">' + data.title + '</h1>' +
+        '<p id="overlay-text">' + data.body + '<p>' +
+      '</section>' +
+      '<menu class="overlay-menu-close">' +
+        '<button class="full js-close-btn" type="button" name="close-btn">' + data.closeButtonText + '</button>' +
+      '</menu>' +
+      '<menu class="overlay-menu-storage">' +
+        '<button class="full js-storage-btn" type="button" name="storage-settings-btn">' + data.storageButtonText + '</button>' +
+      '</menu>' +
+    '</form>';
+  },
+
+  data: function(key, value) {
+    switch (arguments.length) {
+      case 1: return this.el.getAttribute('data-' + key);
+      case 2: this.el.setAttribute('data-' + key, value);
     }
-  });
+  },
+
+  onButtonClick: function(event) {
+    var el  = event.currentTarget;
+    var name = el.getAttribute('name');
+    this.emit('click:' + name);
+  }
+});
+
 });
