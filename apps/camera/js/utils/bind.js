311893ec0cae54ac3ce5b6b55e0da3567c3af8cd
Wilson Page <wilsonpage@me.com>
2013-12-16 18:15:25 +0000

    Consistent formatting

--- apps/camera/js/utils/bind.js
+++ apps/camera/js/utils/bind.js
@@ -1,28 +1,33 @@
-/*global define*/
+define(function(require, exports, module) {
+'use strict';
 
-define(function(require) {
-  'use strict';
 
-  /**
-   * addEventListener shorthand.
-   * @param  {Element}   el
-   * @param  {String}   name
-   * @param  {Function} fn
-   */
-  var exports = function(el, name, fn, context) {
-    if (context) fn = fn.bind(context);
-    el.addEventListener(name, fn);
-  };
+/**
+ * Exports
+ */
 
-  /**
-   * removeEventListener shorthand.
-   * @param  {Element}   el
-   * @param  {String}   name
-   * @param  {Function} fn
-   */
-  exports.unbind = function(el, name, fn) {
-    el.removeEventListener(name, fn);
-  };
+exports = module.exports = bind;
 
-  return exports;
+/**
+ * addEventListener shorthand.
+ * @param  {Element}   el
+ * @param  {String}   name
+ * @param  {Function} fn
+ */
+function bind(el, name, fn, context) {
+  if (context) { fn = fn.bind(context); }
+  el.addEventListener(name, fn);
+}
+
+/**
+ * removeEventListener shorthand.
+ * @param  {Element}   el
+ * @param  {String}   name
+ * @param  {Function} fn
+ */
+exports.unbind = function(el, name, fn) {
+  el.removeEventListener(name, fn);
+};
+
+
 });
