f9e6cb7304a3284c143625e300ff4dd9126b8789
Wilson Page <wilsonpage@me.com>
2014-07-15 16:05:02 -0700

    WIP

--- shared/js/icons.js
+++ shared/js/icons.js
@@ -1,15 +1,14 @@
 'use strict';
 
 window.addEventListener('load', function() {
-//
-// (function() {
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = '/shared/style/icons.css';
-  document.head.appendChild(link);
-  link.addEventListener('load', function() {
-    document.body.classList.add('icons-loaded');
-  });
-// })();
+  // setTimeout(function() {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/shared/style/icons.css';
+    document.head.appendChild(link);
+    // link.addEventListener('load', function() {
+    //   document.body.classList.add('icons-loaded');
+    // });
+  // }, 1000);
 });
