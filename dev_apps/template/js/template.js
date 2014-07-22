f77e9e639cee7c8ce7dcf14403fcebc21dab97d1
Wilson Page <wilsonpage@me.com>
2014-07-22 09:52:29 +0100

    Icon test app

--- dev_apps/template/js/template.js
+++ dev_apps/template/js/template.js
@@ -1,1 +1,10 @@
-// App script goes here.
+'use strict';
+
+window.addEventListener('load', function() {
+  var loading = window.performance.timing.domLoading;
+  var loaded = window.performance.timing.domComplete;
+
+  console.log('loaded: %s', loaded - loading);
+  console.log(window.performance);
+
+});
